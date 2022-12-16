3905905d9a7e3e87be5c470236a9c7b41543f9ef
Hieu Nguyen <hieu.nguyen301202@gmail.com>
2022-12-17 00:29:36 +0700

    update code and comment line

--- Core/Inc/button.h
+++ Core/Inc/button.h
@@ -9,21 +9,29 @@
 #define INC_BUTTON_H_
 
 #include "main.h"
-
+//-------------define port button----------------
 #define Button1 	Button_1_GPIO_Port
 #define Button2		Button_2_GPIO_Port
 #define Button3		Button_3_GPIO_Port
+//#define button N...
 
+//-------------define pin button----------------
 #define B1_PIN		Button_1_Pin
 #define B2_PIN		Button_2_Pin
 #define B3_PIN		Button_3_Pin
+//#define button N...
+
+//------------------ do not change code line from here-----------------
 #define NORMAL_STATE SET
 #define PRESSED_STATE RESET
-#define NUM_OF_BUTTONS 3
 #define TIME_HOLD 100
 #define TIME_AUTO 100
-//normal press
+//----- to here---
 
+//----- number of button------------- <can change>
+#define NUM_OF_BUTTONS 3
+
+//normal press
 int is_button_Pressed(int index) ;
 //long press
 int is_button_long_Pressed(int index) ;
--- Core/Src/button.c
+++ Core/Src/button.c
@@ -8,20 +8,22 @@
 
 
 #include "button.h"
+/* 3 button corresponds to 3 flag of normal press and 3 flag of long press
+ * add more button if you need
+ * something you must be change if you add more button
+ * that place is marked with (*)
+ * */
+int nor_flag[NUM_OF_BUTTONS] = {0, 0, 0};	// (*)
+int long_flag[NUM_OF_BUTTONS] ={0, 0, 0};	// (*)
 
-int nor_flag[NUM_OF_BUTTONS] = {0, 0, 0};
-int long_flag[NUM_OF_BUTTONS] ={0, 0, 0};
+int KeyReg0[NUM_OF_BUTTONS] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};	// (*)
+int KeyReg1[NUM_OF_BUTTONS] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};	// (*)
+int KeyReg2[NUM_OF_BUTTONS] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};	// (*)
+int KeyReg3[NUM_OF_BUTTONS] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};	// (*)
 
-int KeyReg0[NUM_OF_BUTTONS] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};
-int KeyReg1[NUM_OF_BUTTONS] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};
-int KeyReg2[NUM_OF_BUTTONS] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};
-int KeyReg3[NUM_OF_BUTTONS] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};
+int TimeOutForKeyPress[NUM_OF_BUTTONS] =  {0,0,0};							// (*)
 
-
-
-int TimeOutForKeyPress[NUM_OF_BUTTONS] =  {0,0,0};
-
-int time_count_up[NUM_OF_BUTTONS] = {TIME_HOLD, TIME_HOLD, TIME_HOLD} ;
+int time_count_up[NUM_OF_BUTTONS] = {TIME_HOLD, TIME_HOLD, TIME_HOLD} ;		// (*)
 
 int is_button_Pressed(int index){
 	if(nor_flag[index] == 1){
@@ -58,7 +60,7 @@ void getKeyInput(){
 	  	if( i == 0 ) KeyReg2[i] = HAL_GPIO_ReadPin(Button1, B1_PIN);
 	  	else if (i == 1) KeyReg2[i] = HAL_GPIO_ReadPin(Button2, B2_PIN);
 	  	else if (i == 2) KeyReg2[i] = HAL_GPIO_ReadPin(Button3, B3_PIN);
-
+	  	//else if (i == N) KeyRegN[i] = HAL_GPIO_ReadPin(your_button_port, your_button_pin);  (*)
 
 	  	if ((KeyReg0[i] == KeyReg1[i]) && (KeyReg1[i] == KeyReg2[i])){
 	  		if (KeyReg3[i] != KeyReg2[i]){
